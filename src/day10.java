e683bfd5be1cd177b9a759199d7b4dce77df47cc
Sweatha <sweathasrir25csc@vetias.ac.in>
2026-06-16 10:50:26 +0530

    day10

--- src/day10.java
+++ src/day10.java
@@ -1,5 +1,5 @@
 import java.util.ArrayList;
-public class day10 {
+/*public class day10 {
     static void main() {
         ArrayList<String> list= new ArrayList<String> ();
         String[] StudentsList={"Menaga","Mithra","Ram","Narthi","Nivetha"};
@@ -16,6 +16,36 @@ public class day10 {
         System.out.println();
         for (String s:  list){
             System.out.println(s);
+        }
+    }
+}*/
+public class Day10 {
+    static void main() {
+        String[] alpha = {"A","B","C","D"};
+        System.out.println(alpha.length);
+        ArrayList<String> list = new ArrayList<String>();
+        list.add("A");
+        list.add("B");
+        list.add("C");
+        list.add("D");
+        for(String s: list) {
+            System.out.print(s);
+        }
+        list.add("E");
+        list.remove("C");
+        System.out.println();
+        System.out.println(list.getFirst());
+        System.out.println(list.getLast());
+        System.out.println();
+        for(String s: list) {
+            System.out.print(s);
+        }
+        System.out.println();
+        System.out.println(list.get(2));
+        list.addAll(list);
+        System.out.println();
+        for(String s: list) {
+            System.out.print(s);
         }
     }
 }
