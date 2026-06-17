893b4c4707eec890253273c20ff1b216aeb1d9ac
Sweatha <sweathasrir25csc@vetias.ac.in>
2026-06-17 16:07:20 +0530

    day11

--- src/day11.java
+++ src/day11.java
@@ -1,30 +1,29 @@
+import java.lang.classfile.Interfaces;
 import java.lang.reflect.Array;
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.Set;
+import java.util.*;
 
-public class day11 {
-/*    static void main() {
-        String name = null;
-        try {
-            name = null;
-            System.out.println(name.charAt(0));
-            System.out.println(45/0);
-        } catch (ArithmeticException e) {
-            System.out.println("Arithmetic Exception");
-        } catch (ArrayIndexOutOfBoundsException e) {
-            System.out.println("Array Index of Bound Exception");
-        } catch (Exception e) {
-            System.out.println("Exception");
-        } finally {
-            System.out.println("This will always execute");
-           // name="";
+/*public class day11 {
+      static void main() {
+            String name = null;
+            try {
+                name = null;
+                System.out.println(name.charAt(0));
+                System.out.println(45/0);
+            } catch (ArithmeticException e) {
+                System.out.println("Arithmetic Exception");
+            } catch (ArrayIndexOutOfBoundsException e) {
+                System.out.println("Array Index of Bound Exception");
+            } catch (Exception e) {
+                System.out.println("Exception");
+            } finally {
+                System.out.println("This will always execute");
+               // name="";
+            }
+            System.out.println("Still running");
+            System.out.println("Name:" + name);
         }
-        System.out.println("Still running");
-        System.out.println("Name:" + name);
-    }
-    */
-   public static void main(String[] args) {
+        */
+    /*public static void main(String[] args) {
        Set<String> set = new HashSet<>();
        set.add("Sweatha");
        set.add("Menaga");
@@ -36,6 +35,19 @@ public class day11 {
            System.out.println(s);
        }
    }
+}*/
+public class day11{
+    public static void main(String[] args) {
+        Map<String, Object> person = new HashMap<>();
+        person.put("Ram",89);
+        person.put("John",78);
+        person.put("Aravindh",67);
+        person.put("Ramesh",90);
+        System.out.println(person.get("percent"));
+        person.entrySet().forEach(entry ->{
+            System.out.println(entry.getKey() + " : " + entry.getValue());
+        });
+    }
 }
 
 
