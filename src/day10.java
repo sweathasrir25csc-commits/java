34070c2a4c5d518594f0d2690d3fd0e48e8d740d
Sweatha <sweathasrir25csc@vetias.ac.in>
2026-06-17 09:42:09 +0530

    day10

--- src/day10.java
+++ src/day10.java
@@ -56,7 +56,7 @@ public class day10 {
    }
 }
  */
-public class day10 {
+/*public class day10 {
     static void main() {
        /* LinkedList<String> queue = new LinkedList<String>();
         queue.add("Sweatha");
@@ -75,7 +75,7 @@ public class day10 {
     }
 }
 */
-        void takeout(ArrayDeque<String> bucket){
+       /* void takeout(ArrayDeque<String> bucket){
             String plate=bucket.pollLast();
             System.out.print("Taking out of plate");
         }
@@ -86,7 +86,49 @@ public class day10 {
         deque.offerLast("p4");
         System.out.println(deque);
         while(deque .size()>0) {
+*/
 
 
-    }
-}
+
+import java.util.LinkedList;
+
+        public class day10 {
+            static class Student {
+                String name;
+                int regNo;
+                int marks;
+                Student(String name, int regNo, int marks) {
+                    this.name = name;
+                    this.regNo = regNo;
+                    this.marks = marks;
+                }
+            }
+            static void removeStudent(LinkedList<Student> students) {
+                System.out.println("Removed : " + students.removeFirst().name);
+            }
+            static void updateStudent(LinkedList<Student> students) {
+                for (Student s : students) {
+                    if (s.name.equals("Deepa")) {
+                        s.marks = 95;
+                    }
+                }
+            }
+            public static void main(String[] args) {
+                LinkedList<Student> students = new LinkedList<>();
+                students.add(new Student("Arun", 101, 80));
+                students.add(new Student("Bala", 102, 75));
+                students.add(new Student("Charan", 103, 90));
+                students.add(new Student("Deepa", 104, 85));
+                students.add(new Student("Esha", 105, 88));
+                removeStudent(students);
+                updateStudent(students);
+                System.out.println("\nStudent Details");
+                for (Student s : students) {
+                    System.out.println(
+                            s.name + " " +
+                                    s.regNo + " " +
+                                    s.marks
+                    );
+                }
+            }
+        }
