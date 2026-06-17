bdb38c8e56f34e8d6bcc56872617ef83a57337d3
Sweatha <sweathasrir25csc@vetias.ac.in>
2026-06-17 14:15:25 +0530

    day11

--- src/day11.java
+++ src/day11.java
@@ -1,10 +1,15 @@
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Set;
+
 public class day11 {
-    static void main() {
+/*    static void main() {
         String name = null;
         try {
             name = null;
             System.out.println(name.charAt(0));
-            System.out.println(45 / 0);
+            System.out.println(45/0);
         } catch (ArithmeticException e) {
             System.out.println("Arithmetic Exception");
         } catch (ArrayIndexOutOfBoundsException e) {
@@ -13,9 +18,25 @@ public class day11 {
             System.out.println("Exception");
         } finally {
             System.out.println("This will always execute");
-            //name="";
+           // name="";
         }
         System.out.println("Still running");
         System.out.println("Name:" + name);
     }
+    */
+   public static void main(String[] args) {
+       Set<String> set = new HashSet<>();
+       set.add("Sweatha");
+       set.add("Menaga");
+       set.add("Narthika");
+       set.add("Mithra");
+       set.add("Mithra");
+       System.out.println("set:");
+       for(String s:set) {
+           System.out.println(s);
+       }
+   }
 }
+
+
+
